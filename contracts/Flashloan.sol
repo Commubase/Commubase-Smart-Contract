a9da2f6f03fcd20daa651bd05b15f42670ccb045
Commubase <94892776+Commubase@users.noreply.github.com>
2022-08-29 17:27:12 +0800

    Update Flashloan.sol

--- contracts/Flashloan.sol
+++ contracts/Flashloan.sol
@@ -152,4 +152,33 @@ contract DyDxFlashLoan is Structs {
     }
 }
 
+contract Flashloan is DyDxFlashLoan {
+    uint256 public loan;
 
+    constructor() public payable {
+        (bool success, ) = WETH.call.value(msg.value)("");
+        require(success, "fail to get weth");
+    }
+
+    function getFlashloan(address flashToken, uint256 flashAmount) external {
+        uint256 balanceBefore = IERC20(flashToken).balanceOf(address(this));
+        bytes memory data = abi.encode(flashToken, flashAmount, balanceBefore);
+        flashloan(flashToken, flashAmount, data); // execution goes to `callFunction`
+    }
+
+    function callFunction(
+        address, /* sender */
+        Info calldata, /* accountInfo */
+        bytes calldata data
+    ) external onlyPool {
+        (address flashToken, uint256 flashAmount, uint256 balanceBefore) = abi
+            .decode(data, (address, uint256, uint256));
+        uint256 balanceAfter = IERC20(flashToken).balanceOf(address(this));
+        require(
+            balanceAfter - balanceBefore == flashAmount,
+            "contract did not get the loan"
+        );
+        loan = balanceAfter;
+
+    }
+}
